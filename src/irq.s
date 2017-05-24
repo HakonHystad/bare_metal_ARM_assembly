d8ff0f567b7c2f10f62e95f3b3113f7c83ab4f2d
HakonHystad <hakonhystad@gmail.com>
2017-05-24 20:33:58 +0200

    added more on ISR

--- src/irq.s
+++ src/irq.s
@@ -1,8 +1,26 @@
 /* handle interrupt requests and branch to the apropriate ISRs */
 	.include "mmap.s"
+
+///////////////////////////////////////////////////////////////////////////////////////
+// literals 
+//////////////////////////////////////////////////////////////////////////////////////
+.equ KBD_CLK_PIN, 2
+
 	
+///////////////////////////////////////////////////////////////////////////////////////
+// declarations 
+//////////////////////////////////////////////////////////////////////////////////////
+.section .text
+	.globl irq	// global interrupt handler
+	.globl ISR_kbd	// ps2 keyboard clock interrupt
+
+///////////////////////////////////////////////////////////////////////////////////////
+// definitions
+//////////////////////////////////////////////////////////////////////////////////////
+
+
 	
-.globl irq
+/*-------------------------------------- global interrupt handler  ---------------------------------------*/
 irq:
 	push {r0,r1,lr}		// save state
 
@@ -32,7 +50,48 @@ PDG2$:
 	/* different ISRs for source 63:32 goes here */
 	// gpio_int[0]..gpio_int[3] <=> GPU_interrupt 49..52, 49 covers all pins of the header
 	tst r0,#1<<(49-32)	// check gpio_int[0], all header pins
-	//blne kbd_irq
+	bne ISR_kbd
 	
 return$:
 	pop {r0,r1,pc}
+
+/*-------------------------------------- keyboard clock interrupt handler  ---------------------------------------*/
+ISR_kbd:
+	// clear event detect bit for GPIO pin 2 (kbd clock)
+
+	push {r0-r4,lr}
+
+	// clear interrupt (by writing 1 to event detect reg)
+	ldr r0, =GPIOaddr
+	mov r1, #1<<KBD_CLK_PIN
+	str r1, [r0,#GPEDS0]
+
+	mov r2,#5		// loop five times
+
+ledLoop$:
+	
+	/* turn on ACT led */
+	mov r0,#47
+	mov r1,#1
+	bl setGPIOpin
+
+	/* wait */
+	ldr r0,=1000000		//wait 1s
+	bl wait
+	
+	/* turn off ACT led */
+	mov r0,#47
+	mov r1,#0
+	bl setGPIOpin
+
+	/* wait */
+	ldr r0,=1000000
+	bl wait
+
+	subs r2,#1
+
+	bge ledLoop$
+
+	pop {r0-r4,pc}
+
+	
--- src/mmap.s
+++ src/mmap.s
@@ -4,7 +4,31 @@
 	///////////////////////////////////////////////////////////////////////////////////////
 	// GPIO
 	//////////////////////////////////////////////////////////////////////////////////////
-	.equ GPIOaddr, 0x3F200000	// the GPIO base address
+	.equ GPIOaddr, 0x3F200000	// the GPIO base address (gpio function select 0)
+	.equ GPSEL1, 0x4		// gpio function select 1
+	.equ GPSEL2, 0x8		// function select 2
+	.equ GPSEL3, 0xC		// function select 3
+
+	.equ GPSET0, 0x1C		// gpio pin output set 0
+	.equ GPSET1, 0x20		// output set 1
+
+	.equ GPCLR0, 0x28		// gpio pin output clear 0
+	.equ GPCLR1, 0x2C		// output clear 1
+
+	.equ GPLEV0, 0x34		// gpio pin level 0
+	.equ GPLEV1, 0x38		// pin level 1
+
+	.equ GPEDS0, 0x40		// gpio event detect status 0
+	.equ GPEDS1, 0x44		// event detect status 1
+
+	.equ GPREN0, 0x4C		// gpio pin rising edge detect enable 0
+	.equ GPREN1, 0x50		// rising edge detect 1
+
+	.equ GPFEN0, 0x58		// gpio pin falling edge detect enable 0
+	.equ GPFEN1, 0x5C		// falling edge detect enable 1
+
+	.equ GPPUD, 0x94		// gpio pin pull-up/down enable
+	
 
 	///////////////////////////////////////////////////////////////////////////////////////
 	// TIMER
